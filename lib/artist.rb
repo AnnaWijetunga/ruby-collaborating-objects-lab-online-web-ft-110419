33b1601f850bb61424d205fbc48b84e3f92bafd7
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-19 18:41:11 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,12 +20,6 @@ class Artist
     self.all.detect {|artist| artist.name == name} || Artist.new(name)
   end
   
-  # def save
-  #   @@all << self # saves all created instances of Artist class in @@all class variable
-  #   self # the thing we just saved gets returned
-  #   # @all.push(self) - works
-  # end 
-  
   def add_song(song) 
     # self.songs << song # Artist.songs reader returns all
     song.artist = self 
